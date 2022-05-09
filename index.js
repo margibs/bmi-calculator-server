9893ba19b8b2154063c2cae04ce2ef161aedbc22
Rumar Gibs Ecaldre <rumargibs@gmail.com>
2022-05-10 01:31:51 +0800

    Added 2 endpoint for BMI calculator API

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// End point for Standard Calculation
+app.post('/bmi-calculator/standard', (req, res) => {
+    const { weight, feet, inches } = req.body;
+
+    let height = (feet * 12) + inches;
+    let bmi = (weight / (height * height) * 703);
+
+    let message = bmiMessage(bmi);
+    res.json({ "bmi" : bmi.toFixed(1), "message" : message});
+});
+
+
+app.post('/bmi-calculator/metric', (req, res) => {
+    const { weight, height  } = req.body; 
+    let bmi = (weight / (height  * height) ) * 10000;
+    
+    let message = bmiMessage(bmi);
+    res.json({ "bmi" : bmi.toFixed(1), "message" : message});
+});
+
+
+const bmiMessage = (bmi) => {
+    
+    let message = '';
+
+    if (bmi < 18.5 ) {
+        message = 'You are Underweight';
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        message = 'You are Normal weight';
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        message = "You are Overweight";
+    } else {
+        message = "You are Obese";
+    }
+
+    return message;
+}
+
+app.listen(5000,() => {
+    console.log(`Server Started on Port http://localhost:5000`);
+});
